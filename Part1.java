14ae28eff445da8a46ce451a4714f0c2b1012f1f
Michael Grooms <micgrooms@aol.com>
2021-03-10 15:11:32 -0500

    part1 of homework

---
+++ Part1.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Part1 {
+    public static void main(String[] args) {
+        System.out.println("This is part one of the homework");
+
+        ///Users/michaelgroomsjr/IdeaProjects/HWD2/src/main/java/homework
+
+
+        
+    }
+}
